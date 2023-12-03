e1d8f5d1bf733e62febaa3ab2d6eda965c4aabc9
Margaret-Kos <ex1219.19210@gmail.com>
2023-12-03 18:15:20 +0100

    Attempts on Rotation Matrix Integration for Layer

--- smpl_registration/models/split_smpl.py
+++ smpl_registration/models/split_smpl.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import torch
 from smplx.body_models import SMPLLayer
 from smplx.utils import SMPLOutput
+from smplx.lbs import batch_rodrigues
 
 
 class SplitSMPL(torch.nn.Module):
@@ -39,13 +40,29 @@ class SplitSMPL(torch.nn.Module):
 
     @property
     def second_stage_params(self) -> list[torch.nn.Parameter]:
-        return [self.transl, self.top_betas, self.global_orient, self.pose]
+        return [self.transl, self.top_betas, self.global_orient, self.pose]     # Hands Dont get Optimised here?
+
+    def aa2rot(self, pose : torch.Tensor) -> torch.Tensor:
+        # Converts Axis-Angle to Rotation Matrix such that Pose can be Passed to Layer
+
+        bone_count = int(pose.shape[1] / 3)
+        rot_mats = []
+
+        for idx in range(bone_count):
+            axis_ang = pose[:, idx * 3 : (idx + 1) * 3] # (B x 3) 
+            rot_mat = batch_rodrigues(axis_ang)         # (B x 3 x 3) - 3x3 rotation matrix 
+            rot_mat = torch.reshape(rot_mat, (1, -1))   # (B x 9)   
+            rot_mats.append(rot_mat)                    # (bone_count x B x 9)
+
+        rot_mats = torch.cat(rot_mats, dim=1)               # (B x bone_count * 9)
+        return rot_mats
 
     def forward(self, **kwargs) -> SMPLOutput:
-        self.layer.forward(
+        return self.layer.forward(
             betas=torch.cat([self.top_betas, self.other_betas], dim=1),
-            body_pose=self.pose[:, 3:],
-            global_orient=self.global_orient,
+            # body_pose=self.pose[:, 3:],               - Global Orient is Not in Here + Hands Should be their Own Thing 
+            body_pose=self.aa2rot(self.pose),
+            global_orient=self.aa2rot(self.global_orient),
             transl=self.transl,
             **kwargs
-        )
+            )
